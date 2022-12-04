c4a8ea5379e1fa66e60ec5c6207e5bdc102b5167
Henry Zhangxiao <44578113+HenryZhangxiao@users.noreply.github.com>
2022-12-03 21:34:01 -0500

    Henry (#2)
    * init engine push
    
    * actual init engine
    
    * init engine
    
    * init engine
    
    * actual engine commit this time
    
    * removed readme from this branch
    
    * added .project and .cproject to .gitignore
    
    * added .project and .cproject to .gitignore
    
    * removed .project and .cproject files
    
    * removed checksum

---
+++ engine.h
@@ -0,0 +1,38 @@
+
+#include <stdint.h>
+#include <sys/iomsg.h>
+
+#define ENGINE_TOGGLE (_IO_MAX+200)
+#define THROTTLE_TOGGLE (_IO_MAX+201)
+#define BRAKES_TOGGLE (_IO_MAX+202)
+#define INDICATOR_TOGGLE (_IO_MAX+203)
+#define AIRBAG_TOGGLE (_IO_MAX+204)
+
+#define SERVER_NAME "start_car"
+
+// Engine toggle message
+typedef struct engine_toggle_msg_t {
+	uint16_t type;
+} engine_toggle_msg_t;
+
+// Throttle toggle message
+typedef struct throttle_toggle_msg_t {
+	uint16_t type;
+} throttle_toggle_msg_t;
+
+// Brake toggle message
+typedef struct brakes_toggle_msg_t {
+	uint16_t type;
+} brakes_toggle_msg_t;
+
+// Indicator toggle message
+typedef struct indicator_toggle_msg_t {
+	uint16_t type;
+	unsigned char left_right;
+} indicator_toggle_msg_t;
+
+// Airbag toggle message
+typedef struct airbag_toggle_msg_t {
+	uint16_t type;
+} airbag_toggle_msg_t;
+
