455d199763be023b28a8b58c53c9d1ce10c9538f
HenryZhangxiao <44578113+HenryZhangxiao@users.noreply.github.com>
2022-12-08 00:40:41 -0500

    Added airbags.c

---
+++ airbags.c
@@ -0,0 +1,35 @@
+/*
+ * airbags.c
+ */
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/neutrino.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+
+#include "engine.h" // defines messages between client and server
+
+int airbags(int argc, char **argv) {
+
+	airbag_toggle_msg_t msg;
+	int server_coid, return_status;
+
+	printf("airbags.c now attempting to connect to engine.c\n");
+
+	/* find our server to get a coid*/
+	if ((server_coid = name_open(SERVER_NAME, 0)) == -1) {
+		return EXIT_FAILURE;
+	}
+	printf("airbags.c successfully connected to engine.c\n");
+
+	/* send a get message to the server to get a shared memory handle from the server */
+	msg.type = AIRBAG_TOGGLE;
+	MsgSend(server_coid, &msg, sizeof(msg), return_status, sizeof(return_status));
+
+	printf("airbags.c returned: %d\n", return_status);
+
+	return EXIT_SUCCESS;
+}
