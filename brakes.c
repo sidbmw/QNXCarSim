b71219eacab836e7680405264f7ddfc12c724d58
Siddharth Natamai <siddharthnatamai@gmail.com>
2022-12-09 17:31:57 -0500

    Replace tabs with spaces

--- brakes.c
+++ brakes.c
@@ -14,38 +14,38 @@
 
 int main(int argc, char **argv) {
 
-	throttle_toggle_msg_t msg;
-	int server_coid, status;
-	char return_status[256];
-	int pressure;
-	
-	if (argc != 2){
-		printf("Error: Must have two arguments\n");
-		exit(EXIT_FAILURE);
-	}
+    throttle_toggle_msg_t msg;
+    int server_coid, status;
+    char return_status[256];
+    int pressure;
 
-	pressure = argv[1];
+    if (argc != 2){
+        printf("Error: Must have two arguments\n");
+        exit(EXIT_FAILURE);
+    }
 
-	printf("brakes.c now attempting to connect to engine.c\n");
+    pressure = argv[1];
 
-	/* find our server to get a coid*/
-	if ((server_coid = name_open(SERVER_NAME, 0)) == -1) {
-		return EXIT_FAILURE;
-	}
-	printf("brakes.c successfully connected to engine.c\n");
+    printf("brakes.c now attempting to connect to engine.c\n");
 
-	/* send a get message to the server to get a shared memory handle from the server */
-	msg.type = BRAKES_TOGGLE;
-	msg.pressure = pressure;
-	status = MsgSend(server_coid, &msg, sizeof(msg), return_status, sizeof(return_status));
+    /* find our server to get a coid*/
+    if ((server_coid = name_open(SERVER_NAME, 0)) == -1) {
+        return EXIT_FAILURE;
+    }
+    printf("brakes.c successfully connected to engine.c\n");
 
-	printf("brakes.c returned: %s\n", return_status);
+    /* send a get message to the server to get a shared memory handle from the server */
+    msg.type = BRAKES_TOGGLE;
+    msg.pressure = pressure;
+    status = MsgSend(server_coid, &msg, sizeof(msg), return_status, sizeof(return_status));
 
-	if (status == -1) {
-		fprintf(stderr, "Error during MsgSend\n");
-		perror(NULL);
-		exit(EXIT_FAILURE);
-	}
+    printf("brakes.c returned: %s\n", return_status);
 
-	return EXIT_SUCCESS;
+    if (status == -1) {
+        fprintf(stderr, "Error during MsgSend\n");
+        perror(NULL);
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
