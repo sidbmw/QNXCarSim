1ad498a982a8e82963a8f69c4efa9125cefc12c9
dnaliu <danna@poplarml.com>
2022-12-09 19:40:17 -0800

    add steering functionality

--- engine.h
+++ engine.h
@@ -8,34 +8,44 @@
 #define BRAKES_TOGGLE (_IO_MAX+202)
 #define INDICATOR_TOGGLE (_IO_MAX+203)
 #define AIRBAG_TOGGLE (_IO_MAX+204)
+#define STEERING_TOGGLE (_IO_MAX+205)
 
 #define SERVER_NAME "start_car"
 
 // Engine toggle message
 typedef struct engine_toggle_msg_t {
-    uint16_t type;
+	uint16_t type;
 } engine_toggle_msg_t;
 
 // Throttle toggle message
 typedef struct throttle_toggle_msg_t {
-    uint16_t type;
-    int pressure;
+	uint16_t type;
+	int pressure;
 } throttle_toggle_msg_t;
 
 // Brake toggle message
 typedef struct brakes_toggle_msg_t {
-    uint16_t type;
-    int pressure;
+	uint16_t type;
+	int pressure;
 } brakes_toggle_msg_t;
 
 // Indicator toggle message
 typedef struct indicator_toggle_msg_t {
-    uint16_t type;
-    unsigned char left_right; //left = 0, right = 1
+	uint16_t type;
+	//left = 0, right = 1
+	unsigned char left_right;
 } indicator_toggle_msg_t;
 
 // Airbag toggle message
 typedef struct airbag_toggle_msg_t {
-    uint16_t type;
+	uint16_t type;
 } airbag_toggle_msg_t;
 
+// Steering
+typedef struct steering_toggle_msg_t {
+	uint16_t type;
+	//angle: 90 degrees - straight
+	//angles: 0 to 89 degrees - left
+	//angles: 91 - 180 - right
+	unsigned short int angle;
+} steering_toggle_msg_t;
